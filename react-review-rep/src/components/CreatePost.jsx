6636bfe939e054cfd32843d156c24c4e37186638
Kyle Thie <kylethie@kyles-air.attlocal.net>
2021-07-31 12:53:03 -0500

    Kyles_branch

--- react-review-rep/src/components/CreatePost.jsx
+++ react-review-rep/src/components/CreatePost.jsx
@@ -1,12 +1,13 @@
 import React from "react";
 import {Link} from "react-router-dom";
+import NavBar from "./NavBar";
 
 const CreatePost = () => {
 
     return (
         <div>
            
-           
+           <NavBar />
             In Create Post
             
         </div>
--- react-review-rep/src/components/Login.jsx
+++ react-review-rep/src/components/Login.jsx
@@ -1,10 +1,12 @@
 import React from "react";
 import {Link} from "react-router-dom";
+import NavBar from "./NavBar";
 
 const Login = () => {
 
     return (
         <div>
+            <NavBar />
             In Login
         </div>
     );
