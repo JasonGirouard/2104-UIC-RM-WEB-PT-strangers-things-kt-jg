20c43754ee6552ce06872432fd70cc69b1eaff40
Kyle Thie <kylethie@kyles-air.attlocal.net>
2021-08-05 20:38:24 -0500

    made fetch for getuserdata

--- react-review-rep/src/components/HomePage.jsx
+++ react-review-rep/src/components/HomePage.jsx
@@ -1,17 +1,29 @@
 import React, { useEffect, useState, useContext } from "react";
 import { Link } from "react-router-dom";
 import NavBar from "./NavBar";
-import { getPosts } from "./utils";
+import { getPosts, getUserPostsAndMessages } from "./utils";
 import { store } from "../state";
 import SinglePost from "./SinglePost";
 
 const HomePage = () => {
   const { state, dispatch } = useContext(store);
   const [posts, setPosts] = useState([]);
+  const [userPosts, setUserPosts] = useState([]);
+  const [userMessages, setUserMessages] = useState([]);
 
   useEffect(async () => {
     const results = await getPosts();
     setPosts(results);
+  }, []);
+
+  useEffect(async () => {
+    
+    if (JSON.parse(localStorage.getItem("isLoggedIn")) !== "") {
+      
+    const token = (localStorage.getItem("token"))
+    const results = await getUserPostsAndMessages(token);
+    console.log(results)
+    }
   }, []);
 
   return (
